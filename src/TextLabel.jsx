822c8607b7f3291ca51f55bd3eef4494762b1394
Hannu Rytkönen <hannu.rytkonen@gmail.com>
2018-08-16 19:10:18 +0300

    New TextField component

---
+++ src/TextLabel.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import addtext from './actions';
+
+const TextLabel = (props) => {
+  const { text } = props;
+  return (
+    <div>
+      <div>
+        {text}
+      </div>
+    </div>
+  );
+};
+
+function mapDispatchToProps(dispatch) {
+  return ({
+    clearText: text => dispatch(addtext(text)),
+  });
+}
+
+const mapStateToProps = (state) => {
+  return { text: state.text };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TextLabel);
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import TextLabel from './TextLabel';
 import TextField from './TextField';
 
 export default function App() {
   return (
     <div>
-      { 'Hello world' }
+      <TextLabel />
       <TextField />
     </div>
   );
