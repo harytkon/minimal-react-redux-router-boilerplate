307768283188849ecde7f032d1ad1b7c9bef36be
Hannu Rytkönen <hannu.rytkonen@gmail.com>
2018-08-30 20:44:35 +0300

    Handle change of radio button

--- src/RadioButtonComp.jsx
+++ src/RadioButtonComp.jsx
@@ -5,34 +5,36 @@ export default class RadioButtonComp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      checkbox: 1,
     };
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleInputChange(event) {
-    console.log(this);
-    console.log(event);
+    this.setState({ checkbox: event.currentTarget.value });
   }
 
   render() {
+    const { checkbox } = this.state;
+    console.log(checkbox);
     return (
       <div>
         <form>
           <div className="radio">
             <label htmlFor="option1">
-              <input name="option1" type="radio" value="option1" checked />
+              <input name="option" type="radio" value="1" onChange={this.handleInputChange} />
               Option 1
             </label>
           </div>
           <div className="radio">
             <label htmlFor="option2">
-              <input name="option2" type="radio" value="option2" />
+              <input name="option" type="radio" value="2" onChange={this.handleInputChange} />
               Option 2
             </label>
           </div>
           <div className="radio">
             <label htmlFor="option3">
-              <input name="option3" type="radio" value="option3" />
+              <input name="option" type="radio" value="3" onChange={this.handleInputChange} />
               Option 3
             </label>
           </div>
