630cc6f382e117caf03d5c1fddcb1bf9533fc471
Hannu Rytkönen <hannu.rytkonen@gmail.com>
2018-09-06 18:31:22 +0300

    Unit test for changing state of reducer

--- test/reducer.test.js
+++ test/reducer.test.js
@@ -1,11 +1,15 @@
 import { expect } from 'chai';
 import { createStore } from 'redux';
 import reducer from '../src/reducer';
+import { TEXTBOX } from '../src/actions';
 
 describe('Test reducer', () => {
   it('Test initial state of the store created by reducer', () => {
     const store = createStore(reducer);
     const state = store.getState();
     expect(state.text).to.be.eql('');
+  });
+  it('Test to change state using reducer', () => {
+    expect(reducer(undefined, { type: TEXTBOX, text: 'unittest' })).to.be.eql({ text: 'unittest' });
   });
 });
